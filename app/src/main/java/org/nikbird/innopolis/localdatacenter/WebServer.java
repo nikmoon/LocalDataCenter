111250c00b43b82c18df9fff08e30b4dbd2a8242
nikbird <nikbird@debian>
2017-07-18 08:37:52 +0300

    In progress

--- app/src/main/java/org/nikbird/innopolis/localdatacenter/WebServer.java
+++ app/src/main/java/org/nikbird/innopolis/localdatacenter/WebServer.java
@@ -1,12 +1,19 @@
 package org.nikbird.innopolis.localdatacenter;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -15,6 +22,12 @@ import fi.iki.elonen.NanoHTTPD;
  */
 
 public class WebServer extends NanoHTTPD {
+
+    public static final String ERROR_READ_DATA = "Reading data error";
+    public static final String ERROR_AUTHENTICATION = "Username or password invalid";
+    public static final String ERROR_EVENT_QUEUE = "Event queue error";
+    public static final String ERROR_INVALID_TOKEN = "Token invalid";
+
 
     private static class UserInfo {
         private String mName;
@@ -29,16 +42,25 @@ public class WebServer extends NanoHTTPD {
         public String getPassword() { return mPassword; }
     }
 
-    private List<UserInfo> users;
-
-    private String mUsername = "Engineer";
-    private String mPassword = "qwerty";
-    private String mToken = "";
+    private List<UserInfo> mUsers;
+    private Map<String, UserInfo> mTokens;
     private SecureRandom mRandom = new SecureRandom();
 
+    {
+        mUsers = new ArrayList<>();
+        mUsers.add(new UserInfo("Engineer", "qwerty"));
+        mTokens = new HashMap<>();
+    }
 
     public WebServer() {
         super("127.0.0.1", 8080);
+        startRandomBreaker();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        mStopBreaker = true;
     }
 
     @Override public Response serve(IHTTPSession session) {
@@ -53,6 +75,10 @@ public class WebServer extends NanoHTTPD {
             case "/auth/":
                 response = authentication(session);
                 break;
+            case "/event":
+            case "/event/":
+                response = event(session);
+                break;
             default:
                 response = textResponse("Unknown path");
                 break;
@@ -60,46 +86,114 @@ public class WebServer extends NanoHTTPD {
         return response;
     }
 
+    private BlockingQueue<String> mEventQueue = new ArrayBlockingQueue<String>(10);
+
+    private Response event(IHTTPSession session) {
+        try {
+            if (!checkToken(session))
+                return errorResponse(ERROR_INVALID_TOKEN);
+        } catch (IOException e) {
+            return errorResponse(ERROR_READ_DATA);
+        }
+
+        try {
+            return textResponse(mEventQueue.take());
+        } catch (InterruptedException e) {
+            return errorResponse(ERROR_EVENT_QUEUE);
+        }
+    }
+
+    private static final int RACK_COUNT = 5;
+    private static final int RACK_CAPACITY = 4;
+
     private Response replication(IHTTPSession session) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(session.getInputStream()));
+        try {
+            if (!checkToken(session))
+                return errorResponse(ERROR_INVALID_TOKEN);
+        } catch (IOException e) {
+            return errorResponse(ERROR_READ_DATA);
+        }
 
-        StringBuilder answer = new StringBuilder();
-        int rackCount = 7;
-        int rackCapacity = 5;
+        StringBuilder response = new StringBuilder();
 
-        answer.append("rack_count:" + rackCount + "\n");
-        for(int rackNum = 0; rackNum < rackCount; rackNum++) {
-            answer.append("rack:" + rackNum + ":" + rackCapacity + "\n");
-            for(int serverNum = 0; serverNum < rackCapacity; serverNum++) {
-                answer.append("GOOD\n");
+        response.append("rack_count:" + RACK_COUNT + "\n");
+        for(int rackNum = 0; rackNum < RACK_COUNT; rackNum++) {
+            response.append("rack:" + rackNum + ":" + RACK_CAPACITY + "\n");
+            for(int serverNum = 0; serverNum < RACK_CAPACITY; serverNum++) {
+                response.append("GOOD\n");
             }
         }
-        return textResponse(answer.toString());
+        return textResponse(response.toString());
+    }
+
+    private boolean checkToken(BufferedReader reader) throws IOException {
+        String token = reader.readLine().split(":")[1];
+        if (mTokens.containsKey(token))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean checkToken(IHTTPSession session) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(session.getInputStream()));
+        return checkToken(reader);
     }
 
     private Response authentication(IHTTPSession session) {
-        String responseText;
-        String username;
-        String password;
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(session.getInputStream()));
         try {
-            username = reader.readLine().split(":")[1];
-            password = reader.readLine().split(":")[1];
-            if (mUsername.equals(username) && mPassword.equals(password)) {
-                String token = new BigInteger(130, mRandom).toString(32);
-                mToken = token;
-                responseText = "token:" + token;
-            } else {
-                responseText = "Username or password invalid";
-            }
+            String username = reader.readLine().split(":")[1];
+            String password = reader.readLine().split(":")[1];
+            UserInfo user = findUser(username);
+            if (user != null && user.getPassword().equals(password)) {
+                String token;
+                do {
+                    token = new BigInteger(130, mRandom).toString(32);
+                } while (mTokens.containsKey(token));
+                mTokens.put(token, user);
+                return textResponse("token:" + token);
+            } else
+                return errorResponse(ERROR_AUTHENTICATION);
         } catch (IOException e) {
-            responseText = "Error reading authentication data";
+            return errorResponse(ERROR_READ_DATA);
         }
-        return textResponse(responseText);
+    }
+
+    private UserInfo findUser(String username) {
+        for (UserInfo user : mUsers) {
+            if (user.getName().equals(username))
+                return user;
+        }
+        return null;
     }
 
     private Response textResponse(String response) {
+        Log.i("LOCAL_DATA_CENTER", response);
         return newFixedLengthResponse(Response.Status.OK, MIME_PLAINTEXT, response);
+    }
+
+    private Response errorResponse(String errorText) {
+        return textResponse("servererror:" + errorText);
+    }
+
+    private volatile boolean mStopBreaker;
+
+    private void startRandomBreaker() {
+        new Thread(new Runnable() {
+            @Override public void run() {
+                Random random = new Random();
+                while (true) {
+                    if (mStopBreaker)
+                        break;
+                    try { Thread.sleep(15000); } catch (InterruptedException e) {}
+                    if (mEventQueue.size() > 5)
+                        continue;
+                    int rackNum = random.nextInt(RACK_COUNT);
+                    int serverNum = random.nextInt(RACK_CAPACITY);
+                    try { mEventQueue.put("server:" + rackNum + ":" + serverNum + ":FAIL"); }
+                    catch (InterruptedException e) {}
+                }
+            }
+        }).start();
     }
 }
